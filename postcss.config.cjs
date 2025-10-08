363b6835c577678b2dc89fdaa1eb7f2b4ef98a51
Adam Snape <adam@snape.xyz>
2025-10-08 22:00:21 +0100

    initial commit

---
+++ postcss.config.cjs
@@ -0,0 +1,15 @@
+// postcss.config.cjs
+
+module.exports = {
+  plugins: {
+    'postcss-preset-env': {
+      // Stage 3 means stable features are enabled.
+      // This includes the CSS Nesting specification.
+      stage: 3,
+
+      // Optionally specify browser support (e.g., last 2 major versions)
+      // This is necessary for some polyfills to work correctly.
+      browsers: 'defaults',
+    },
+  },
+};
